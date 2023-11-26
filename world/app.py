f5ae1bfa4f2199f7412aa91c5a55b1019ef01b5d
Sylwester Mielniczuk <flaboy.com@gmail.com>
2023-11-26 01:30:02 +0000

    Controllers and Hands

--- world/app.py
+++ world/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory, render_template
+from flask import Flask, request, jsonify, send_from_directory, render_template, url_for
 import asyncio
 import threading
 import json
